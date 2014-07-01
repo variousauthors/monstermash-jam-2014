58b145bc0f6a0bb82372bba926ce966278cd17ff
Zeigfreid <various.authors@gmail.com>
2014-06-30 22:56:38 -0700

    Added a state machine to govern animation states

--- player.lua
+++ player.lua
@@ -1,10 +1,52 @@
 if not Entity then require("entity") end
 
+LEFT  = 0
+RIGHT = 1
+
 return function (x, y)
     local entity    = Entity()
     local p         = Point(x, y)
     local will_move = nil
     local maneuver  = nil
+    local facing    = RIGHT
+    local movement  = FSM()
+
+    movement.addState({
+        name = "running"
+    })
+
+    movement.addState({
+        name = "standing"
+    })
+
+    movement.addTransition({
+        from = "standing",
+        to = "running",
+        condition = function ()
+            return entity.get("left") or entity.get("right")
+        end
+    })
+
+    movement.addTransition({
+        from = "running",
+        to = "standing",
+        condition = function ()
+            return not entity.get("left") and not entity.get("right")
+        end
+    })
+
+    movement.start("standing")
+
+    local controls = {
+        left  = function ()
+            p.setX(p.getX() - 10)
+            facing = LEFT
+        end,
+        right = function ()
+            p.setX(p.getX() + 10)
+            facing = RIGHT
+        end
+    }
 
     local willMove = function ()
         return will_move ~= nil
@@ -17,11 +59,30 @@ return function (x, y)
         end
     end
 
-    entity.update     = function (dt, timer)
+    entity.update = function (dt)
+        movement.update()
+
+        for k, v in pairs(controls) do
+            if entity.get(k) then
+                v(dt)
+            end
+        end
     end
 
     entity.draw       = function ()
-        love.graphics.setColor(COLOR.BLUE)
+        love.graphics.setColor(COLOR.WHITE)
+        if facing == LEFT then
+            love.graphics.line(p.getX(), p.getY(), p.getX(), p.getY() + 10)
+        else
+            love.graphics.line(p.getX() + 10, p.getY(), p.getX() + 10, p.getY() + 10)
+        end
+
+        if movement.is("running") then
+            love.graphics.setColor(COLOR.RED)
+        else
+            love.graphics.setColor(COLOR.BLUE)
+        end
+
         love.graphics.rectangle("fill", p.getX(), p.getY(), 10, 10)
     end
 
