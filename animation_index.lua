1040708d2d41b000e5979d5b2d494b9e349a1642
Zeigfreid <various.authors@gmail.com>
2014-07-06 18:36:05 -0700

    Added the indices for all animations
    based on the spreadsheet

--- animation_index.lua
+++ animation_index.lua
@@ -1,11 +1,32 @@
 
 animation_index = {
-    standing      = { 1, 1 },
-    into_jumping  = { '2-4', 1 },
-    jumping       = { 4,1 },
-    into_falling  = { '4-6', 1 },
-    falling       = { 6,1 },
-    into_standing = { '7-8', 1 },
+    standing            = { 1, 1 },
+
+    to_recoil = { 1,2 },
+    recoil    = { 2,2 },
+
+    to_jumping          = { '2-4', 1 },
+    jumping             = { 4,1 },
+    to_falling          = { '4-6', 1 },
+    falling             = { 6,1 },
+    falling_to_standing = { '7-8', 1 },
+
+    to_running   = { 1,5 },
+    running      = { '2-11', 5 },
+    from_dashing = { 1,5 },
+
+    to_dashing   = { 1,7 },
+    dashing      = { 2,7 },
+    from_dashing = { 1,7 },
+
+    to_climbing      = { '1-2', 9 },
+    climbing         = { 3,9 },
+    climbing_to_jump = { 4,9 },
+
+    to_hurt = { 1,11 },
+    hurt    = { '2-11', 11 },
+
+    death = { 1,11 }
 }
 
 animation_index.get = function (string)
